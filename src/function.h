784df93a14f336d9e8b94ea918ae02fefd181e8b
showc7 <showc7@gmail.com>
2013-05-14 17:36:50 +0300

    new file:   .gitignore 	new file:   src/function.cpp 	new file:   src/function.h 	modified:   src/main.cpp

---
+++ src/function.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_H
+#define FUNCTIONS_H
+
+int sum(int a, int b);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "function.h"
 
 int main()
 {
 	printf("hello world !!!\n");
+
+	int a = sum(7,7);
 
 	return 0;
 }
